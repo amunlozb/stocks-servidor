cf81692d59573f618ead822b160157393398acce
amunlozb <amunlozb@g.educaand.es>
2024-03-01 10:18:05 +0100

    Añadida documentación a los tests

--- src/test/java/com/amunlozb/stocksservidor/StockTests.java
+++ src/test/java/com/amunlozb/stocksservidor/StockTests.java
@@ -15,26 +15,31 @@ class StockTests {
 
     private Stock stock;
 
+    // Antes de cada test, se inicializa un Stock con los valores especificados
     @BeforeEach
     public void setUp() {
         stock = new Stock("ticker_Accion", "nombre_Accion", "moneda_Accion", "local_Accion");
     }
 
+    // Test para comprobar que el ticker devuelto por el getter es el esperado
     @Test
     public void test_ticker() {
         assertEquals("ticker_Accion", stock.getTicker());
     }
 
+    // Test para comprobar que el nombre devuelto por el getter es el esperado
     @Test
     public void test_nombre() {
         assertEquals("nombre_Accion", stock.getName());
     }
 
+    // Test para comprobar que la moneda devuelta por el getter es la esperada
     @Test
     public void test_moneda() {
         assertEquals("moneda_Accion", stock.getCurrency_name());
     }
 
+    // Test para comprobar que el locale devuelto por el getter es el esperado
     @Test
     public void test_local() {
         assertEquals("local_Accion", stock.getLocale());
